7febe392340527741dc82e6ca3b0d279b3de4691
BastosDev <gustavoporto.gp63@gmail.com>
2026-04-23 16:20:11 -0300

    add .venv and inputs

---
+++ main.py
@@ -0,0 +1,6 @@
+
+
+print('Triad Online')
+# user name
+usr_name = input('Enter your username : ')
+print(f'Hi {usr_name}, Triad is ready to help you.')
---
+++ studypy/conversions.py
@@ -0,0 +1,9 @@
+# Conversions types
+
+string = '10'
+
+stringToInt = int(string)
+stringToFloat = float(string)
+
+print(f'{stringToFloat} (float)')
+print(f'{stringToInt} (int)')
