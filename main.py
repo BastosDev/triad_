54648d2f90d0b39d9c408b81313edb35b7ce8b84
BastosDev <gustavoporto.gp63@gmail.com>
2026-04-23 17:07:14 -0300

    refining the code

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ config = {
 }
 
 print('Accessing keys')
-print(f'{config['name']}')
-print(f'{config['user']}')
-print(f'{config['default_min_focus']}')
+print(f'{config["name"]}')
+print(f'{config["user"]}')
+print(f'{config["default_min_focus"]}')
 
 new_time = input('Type the new focus time: ')
-config['default_min_focus'] = int(new_time)
+config["default_min_focus"] = int(new_time)
 
-print(f'New focus time is defined to: {config['default_min_focus']}')
+print(f'New focus time is defined to: {config["default_min_focus"]}')
 
 def get_config(key):
-    return config.get('key')
+    return config.get(key)
